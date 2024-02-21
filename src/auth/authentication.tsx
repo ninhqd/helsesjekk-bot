e39e096835b8595b7d551a4dd4238ca6742b2e1c
Ninh Qui Do <doquininh@gmail.com>
2024-02-21 12:20:52 +0100

    no more danger

--- src/auth/authentication.tsx
+++ src/auth/authentication.tsx
@@ -77,7 +77,6 @@ export async function validateToken(redirectPath: string): Promise<void> {
 export async function getToken(): Promise<string> {
   if (isLocal) return fakeToken;
   const session = await getServerSession(authOptions);
-  console.log(session.accessToken);
   return session.accessToken;
 }
 
