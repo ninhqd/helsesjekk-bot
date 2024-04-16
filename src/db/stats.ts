25e51e8663bdd317b95df26d3c442a4a95953771
Ninh Do <ninh.qui.do@politiet.no>
2024-04-16 14:34:29 +0200

    debug

--- src/db/stats.ts
+++ src/db/stats.ts
@@ -8,23 +8,19 @@ export async function funStats(): Promise<{
   dashboardTeams: number;
   mostQuestions: number;
 }> {
-  const [
-    activeTeams,
-    totalAsks,
-    totalAnswers,
-    biggestTeam,
-    dashboardTeams,
-    mostQuestions,
-  ] = await Promise.all([
-    await prisma.team.count({
-      where: { active: true },
-    }),
-    await prisma.asked.count({
-      where: { revealed: true, skipped: false },
-    }),
-    await prisma.answer.count({
-      where: { asked: { revealed: true, skipped: false } },
-    }),
+  const activeTeams = await prisma.team.count({
+    where: { active: true },
+  });
+  console.debug(activeTeams);
+  const totalAsks = await prisma.asked.count({
+    where: { revealed: true, skipped: false },
+  });
+  console.debug(totalAsks);
+  const totalAnswers = await prisma.answer.count({
+    where: { asked: { revealed: true, skipped: false } },
+  });
+  console.debug(totalAnswers);
+  const biggestTeam =
     (
       await prisma.asked.findFirst({
         orderBy: {
@@ -34,14 +30,17 @@ export async function funStats(): Promise<{
           _count: { select: { answers: true } },
         },
       })
-    )._count.answers || 0,
-    await prisma.team.count({
-      where: { active: true, assosiatedGroup: { not: null } },
-    }),
-    (
-      await prisma.$queryRaw`SELECT MAX(jsonb_array_length(questions)) FROM "Team";`
-    )[0]?.max,
-  ]);
+    )?._count.answers || 0;
+  console.debug(biggestTeam);
+  const dashboardTeams = await prisma.team.count({
+    where: { active: true, assosiatedGroup: { not: null } },
+  });
+  console.debug(dashboardTeams);
+
+  const mostQuestions = (
+    await prisma.$queryRaw`SELECT MAX(jsonb_array_length(questions)) FROM "Team";`
+  )[0]?.max;
+  console.debug(mostQuestions);
 
   return {
     activeTeams,
