7ffa190a8024cf0f5282b22f87e5f90c1dd3d3e8
Ninh Do <ninh.qui.do@politiet.no>
2024-04-16 13:25:24 +0200

    debug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ export const metadata: Metadata = {
 export default async function Page(): Promise<ReactElement> {
   return (
     <div>
-      {/* <Teams /> */}
-      <Stats />
+      <Teams />
+      {/* <Stats /> */}
     </div>
   );
 }
